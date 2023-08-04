c86a4c42089b9d0dbf04323592c07a67d7b1eeb0
Monique Caldeira <nique.sca@gmail.com>
2023-08-04 14:35:03 -0300

    implementação da função para mudar o tamanho do brush, função da borracha e função para apagar a tela - projeto já está funcionando

--- index.js
+++ index.js
@@ -4,32 +4,89 @@ const ctx = canvas.getContext('2d');
 
 const inputColor = document.querySelector('.input__color');
 const tools = document.querySelectorAll('.button__tool');
-const sizeButtons = document.querySelector('.button__size');
+const sizeButtons = document.querySelectorAll('.button__size');
 const buttonClear = document.querySelector('.button__clear');
 
 let brushSize = 10;
-ctx.fillStyle = "#000"
 let isPaining = false;
 
-canvas.addEventListener("mousedown", (event)=>{
-   const {clientX, clientY} = event;
-   draw(clientX, clientY);
+let activeTool = "brush";
+
+inputColor.addEventListener("change", ({ target }) => {
+   ctx.fillStyle = target.value
+});
+
+canvas.addEventListener("mousedown", (event) => {
+   const { clientX, clientY } = event;
    isPaining = true;
-})
-
-canvas.addEventListener("mouseup", (event)=>{
-   isPaining = false;
-})
-
-canvas.addEventListener("mousemove", ({clientX, clientY})=>{
-   if(isPaining){
+   if (activeTool === "brush") {
       draw(clientX, clientY);
+   }
+   if (activeTool === "rubber") {
+      erase(clientX, clientY);
    }
 })
 
-const draw = (x,y) =>{
+canvas.addEventListener("mouseup", (event) => {
+   isPaining = false;
+})
+
+canvas.addEventListener("mousemove", ({ clientX, clientY }) => {
+   if (isPaining && activeTool === "brush") {
+      draw(clientX, clientY);
+   }
+   if (isPaining && activeTool === "rubber") {
+      erase(clientX, clientY);
+   }
+})
+
+const draw = (x, y) => {
+   ctx.globalCompositeOperation = "source-over"
    ctx.beginPath()
-   ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize/2, 0, 90);
+   ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize / 2, 0, 90);
    ctx.fill();
    //ctx.fillRect(x - canvas.offsetLeft, y - canvas.offsetTop,brushSize, brushSize);
 }
+
+const erase = (x, y) => {
+   ctx.globalCompositeOperation = "destination-out";
+   ctx.beginPath()
+   ctx.arc(x - canvas.offsetLeft, y - canvas.offsetTop, brushSize / 2, 0, 90);
+   ctx.fill();
+   //ctx.fillRect(x - canvas.offsetLeft, y - canvas.offsetTop,brushSize, brushSize);
+}
+
+
+tools.forEach((tool) => {
+   tool.addEventListener("click", ({ target }) => {
+      //#Closest vai buscar um elemento que está mais próximo do target (button-tools)
+      const selectedTool = target.closest("button");
+      const action = selectedTool.getAttribute("data-action");
+      if (action) {
+         tools.forEach((tool) => {
+            tool.classList.remove("active");
+         })
+         activeTool = action;
+         selectedTool.classList.add("active");
+      }
+   })
+})
+
+sizeButtons.forEach((buttons) => {
+   buttons.addEventListener("click", ({ target }) => {
+      //#Closest vai buscar um elemento que está mais próximo do target (button-buttons)
+      const selectedButton = target.closest("button");
+      const size = selectedButton.getAttribute("data-size");
+
+      sizeButtons.forEach((buttons) => {
+         buttons.classList.remove("active");
+         brushSize = size;
+         selectedButton.classList.add("active");
+      })
+   })
+})
+
+
+buttonClear.addEventListener("click",()=>{
+   ctx.clearRect(0, 0, canvas.width,canvas.height);
+})
